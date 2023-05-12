72e6fc4a3a6fb7cd9e01f2c66d6328b4d9bb1814
Admin <Admin@DESKTOP-34GRT87>
2023-05-12 13:54:24 +0300

    Add project files.

---
+++ HW_14/Program.cs
@@ -0,0 +1,35 @@
+﻿var n = 3;
+
+Semaphore semaphore = new Semaphore(1, 2);
+
+Thread thread1 = new Thread(Foo);
+Thread thread2 = new Thread(Bar);
+
+thread1.Start();
+thread2.Start();
+
+Console.ReadLine();
+
+void Foo()
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        semaphore.WaitOne();
+        Console.Write("foo");
+        semaphore.Release();
+    }
+
+}
+
+void Bar()
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        semaphore.WaitOne();
+        Console.Write("bar");
+        semaphore.Release();
+    }
+
+}
